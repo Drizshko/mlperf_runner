a39bbee9eadeefa7b4273e6cf6b965a3f661f169
Dmitry Rizshkov <dmitry.rizshkov@intel.com>
2021-03-18 13:01:36 -0700

    Added comments

--- run_mlperf.py
+++ run_mlperf.py
@@ -20,7 +20,9 @@ import sys
 import argparse
 from shutil import copyfile
 import json
-import shlex, subprocess
+import shlex
+import subprocess
+import selectors
 
 
 def getInputParameters():
@@ -31,12 +33,18 @@ def getInputParameters():
 
 def run_process(args, dir, env, redirect_output):
     output = ""
-    with subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True, cwd=dir, env=env) as p:
-        for line in p.stdout:
-            if redirect_output:
-                print(line, end='')
-            output += line
-    return output
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, 
+                         bufsize=1, universal_newlines=True, cwd=dir, env=env) 
+    while True:
+      data = process.stdout.readline()
+      if data == '' and process.poll() is not None :
+        break
+      if data:
+        output += data
+        if redirect_output:
+            print(data, end="")
+    rc = process.poll()
+    return rc, output
 
 
 def expand_paths(cmd, dct):
@@ -49,18 +57,31 @@ def str2bool(v):
   return v.lower() in [ 'yes', 'true', 't', '1' ]
 
 
+def create_env(config):
+    run_env = os.environ.copy()
+    if 'env' in config['config']:
+        export = config['config']['env']
+        for item in export:
+            key = list(item.keys())[0] 
+            run_env[key] = item[key]
+    return run_env
+
+
+def create_folder(path, name):
+    if not os.path.isdir(path):
+        print('Creating {} folder: {}'.format(name, path))
+        os.makedirs(path)
+    else:
+        print('{} folder exists: {}'.format(name, path))
+
+
 def main():
     args = getInputParameters().parse_args()
 
     with open(args.config, 'r') as f:
         config_dict = json.load(f)
 
-    run_env = os.environ.copy()
-
-    if 'export' in config_dict['config']:
-        val = config_dict['config']['export'].split('=')
-        if len(val) == 2:
-            run_env[val[0]] = val[1]
+    run_env = create_env(config_dict)
 
     logs_path = config_dict['config']['logs_dir']
 
@@ -95,18 +116,11 @@ def main():
         aliases = config_dict['config']['aliases']
     
     aliases['$LOGS_DIR'] = logs_path
-    aliases['$COMPLIANCE_OUTPUT_DIR'] = config_dict['config']['compliance_output_dir']
-
-    if not os.path.isdir(org):
-        os.makedirs(org)
-        print('Creating org folder: {}'.format(org))
 
     code_path = os.path.join(code, framework, net)
-    if not os.path.isdir(code_path):
-        print('Creating code folder: {}'.format(code_path))
-        os.makedirs(code_path)
-    else:
-        print('Code folder exists: {}'.format(code_path))
+
+    create_folder(org, 'org')
+    create_folder(code_path, 'code')
 
     compliance_tests = [ 'TEST01', 'TEST04-A', 'TEST04-B', 'TEST05' ]
     compliance_config_path = os.path.join(mlperf_path, 'compliance', 'nvidia')
@@ -114,15 +128,15 @@ def main():
 
     for test in compliance_tests:
         compliance_path = os.path.join(compliance, system, net, scenario, test)
+        compliance_tmp_path = os.path.join(compliance, system, net, scenario, 'tmp', test)
         compliance_paths[test] = compliance_path
         aliases['${}_DIR'.format(test)] = compliance_path
-        if not os.path.isdir(compliance_path):
-            print('Creating {} folder: {}'.format(test, compliance_path))
-            os.makedirs(compliance_path)
-        else:
-            print('{} folder exists: {}'.format(test, compliance_path))
-        
-        audit_dst = os.path.join(compliance_path, 'audit.config')
+        aliases['${}_TMP_DIR'.format(test)] = compliance_tmp_path
+
+        create_folder(compliance_path, test)
+        create_folder(compliance_tmp_path, test)
+
+        audit_dst = os.path.join(compliance_tmp_path, 'audit.config')
 
         if test == 'TEST01':
             audit_conf = os.path.join(compliance_config_path, test, net_name, 'audit.config')
@@ -138,11 +152,8 @@ def main():
     aliases['$COMPLIANCE_DIR'] = os.path.join(compliance, system, net, scenario)
 
     measurements_path = os.path.join(measurements, system, net, scenario)
-    if not os.path.isdir(measurements_path):
-        print('Creating measurements folder: {}'.format(measurements_path))
-        os.makedirs(measurements_path)
-    else:
-        print('Measurements folder exists: {}'.format(measurements_path))
+
+    create_folder(measurements_path, 'measurements')
 
     mlperf_dst_conf = os.path.join(measurements_path, 'mlperf.conf')
     if not os.path.isfile(mlperf_dst_conf):
@@ -152,47 +163,43 @@ def main():
         print('mlperf.conf file exists: {}'.format(mlperf_dst_conf))
 
     results_path = os.path.join(results, system, net, scenario)
-    if not os.path.isdir(results_path):
-        print('Creating results folder: {}'.format(results_path))
-        os.makedirs(results_path)
-    else:
-        print('Results folder exists: {}'.format(results_path))
+
+    create_folder(results_path, 'results')
 
     aliases['$RESULTS_DIR'] = results_path
 
     results_type = [ 'performance/run_1', 'accuracy' ]
     for typ in results_type:
         result_path = os.path.join(results_path, typ)
-        if not os.path.isdir(result_path):
-            print('Creating results folder: {}'.format(result_path))
-            os.makedirs(result_path)
-        else:
-            print('Results folder exists: {}'.format(result_path))
+        create_folder(result_path, typ)
         aliases['${}_DIR'.format(typ.upper())] = result_path
 
     systems_path = os.path.join(org, 'systems')
-    if not os.path.isdir(systems_path):
-        print('Creating systems folder: {}'.format(systems_path))
-        os.makedirs(systems_path)
-    else:
-        print('Systems folder exists: {}'.format(systems_path))
+    create_folder(systems_path, 'systems')
 
     if 'exec' in config_dict['config']:
         for exec in config_dict['config']['exec']:
             print('Running {}'.format(exec['name']))
-            dir = expand_paths(exec['dir'], aliases)
+            if 'dir' in exec:
+                dir = expand_paths(exec['dir'], aliases)
+            else:
+                dir = None
             cmd = expand_paths(exec['cmd'], aliases)
             if 'skip' in exec:
-                if bool(exec['skip']):
+                if str2bool(exec['skip']):
                     print('Skipping...')
                     continue
             args = shlex.split(cmd)
-            output = run_process(args, dir, run_env, redirect_output)
+            code, output = run_process(args, dir, run_env, redirect_output)
             if 'save_output' in exec:
                 path = os.path.join(dir, exec['save_output'])
                 f = open(path, 'w+')
                 f.write(output)
                 f.close()
+            
+            if not code == 0:
+                print('Error returned...')
+                break
 
 
 if __name__ == '__main__':
